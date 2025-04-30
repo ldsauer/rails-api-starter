7e58c51ae30c284147170ef4c7cf641096a6284f
Logan Sauer <ldsauer2@gmail.com>
2025-04-30 13:01:13 -0600

    installed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'  # ← In dev, this is okay. You can restrict it later if needed.
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :patch, :put, :delete, :options, :head]
+  end
+end
